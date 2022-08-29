bc6d60385fea78a6ee5883d8bbb644c35f9671ab
Ghost Agent 3 <nahashkuria97@gmail.com>
2022-08-29 09:18:47 +0300

    Vi requirment - 13. Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,20 +1,37 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
- * @tree: A pointer to the root node of the tree to count the number of nodes.
+ * parent_counter - Computes the number of non-leaf nodes in a node.
+ * @tree: The binary tree.
+ * @n: A pointer to the tree's non-leaf node value.
+ */
+void parent_counter(const binary_tree_t *tree, size_t *n)
+{
+	if (tree != NULL)
+	{
+		if ((tree->left != NULL) || (tree->right != NULL))
+		{
+			if (n != NULL)
+			{
+				(*n)++;
+				parent_counter(tree->left, n);
+				parent_counter(tree->right, n);
+			}
+		}
+	}
+}
+
+/**
+ * binary_tree_nodes - Computes the number of non-leaf nodes \
+ * in a binary tree.
+ * @tree: The binary tree.
  *
- * Return: If tree is NULL, the function must return 0, else return node count.
+ * Return: The number of non-leaf nodes in the given binary tree.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes = 0;
+	size_t n = 0;
 
-	if (tree)
-	{
-		nodes += (tree->left || tree->right) ? 1 : 0;
-		nodes += binary_tree_nodes(tree->left);
-		nodes += binary_tree_nodes(tree->right);
-	}
-	return (nodes);
+	parent_counter(tree, &n);
+	return (n);
 }
