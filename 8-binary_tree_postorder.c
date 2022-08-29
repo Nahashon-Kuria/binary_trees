6a26147c18bd77b26aba0444d77f2a21618dad48
Ghost Agent 3 <nahashkuria97@gmail.com>
2022-08-29 09:06:28 +0300

    Vi requirment - 8. Post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - Traverses a binary tree using post-order traversal.
- * @tree: A pointer to the root node of the tree to traverse.
- * @func: A pointer to a function to call for each node.
+ * binary_tree_postorder - Performs a preorder traversal on a tree.
+ * @tree: The tree to traverse.
+ * @func: The function to handle the traversed node's value.
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree && func)
+	if ((tree != NULL) && (func != NULL))
 	{
 		binary_tree_postorder(tree->left, func);
 		binary_tree_postorder(tree->right, func);
